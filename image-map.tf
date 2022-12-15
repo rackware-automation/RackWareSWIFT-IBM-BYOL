95ca98526cde01b1545fc2096acdabaf39b23c96
rackware-automation <106510095+rackware-automation@users.noreply.github.com>
2022-12-15 11:36:55 +0530

    Update image-map.tf for 1.3.0.159
    Updated image-map.tf for 1.3.0.159.

--- image-map.tf
+++ image-map.tf
@@ -1,13 +1,13 @@
-locals {
-  image_map = {
-    "au-syd"   = "r026-e2db604f-eba3-4cdd-979c-d3b92c23b9c0"
-    "br-sao"   = "r042-3ab7faf7-1cbb-481c-9b8d-9e71327dacef"
-    "ca-tor"   = "r038-7ea0f6e5-7354-4fb1-ab97-25884b49736e"
-    "eu-de"    = "r010-82a9bc67-e8f1-4237-99a7-37a4b38af6b7"
-    "eu-qb"    = "r018-f58f4883-46d5-4e17-9423-e4272d3881ed"
-    "jp-osa"   = "r034-21564963-e26e-4e91-bac1-9e3e877d2746"
-    "jp-tok"   = "r022-703a8d65-ce93-4888-92c0-92e72fd3409c"
-    "us-east"  = "r014-c755fc27-8dcb-4c00-822e-c4061dc69255"
-    "us-south" = "r006-4d09472b-5dc9-49d5-a588-cb880db8dc52"
-  }
-}
+locals { 
+  image_map = { 
+    "us-south" = "r006-0ea38abd-b404-4c99-84b0-37a2f16b077a" 
+    "us-east"  = "r014-feda2ff7-655a-4292-b6aa-b447194fd52d" 
+    "eu-gb"    = "r018-0c040cf2-3449-47e0-9938-d3410fc3230a" 
+    "eu-de"    = "r010-fd76bb49-4cda-4fe1-ad2a-89d88737a606" 
+    "jp-tok"   = "r022-56653ed8-63f7-478d-a17a-6a59b17bf154" 
+    "jp-osa"   = "r034-68b414f7-d419-4592-9eb3-8510af2cdd3d" 
+    "au-syd"   = "r026-eec0deb0-45c2-4755-83aa-ff2f5369871e" 
+    "ca-tor"   = "r038-fd9b70ee-bc4b-4b9a-a19a-89af59f6e1b5" 
+    "br-sao"   = "r042-19442649-fb3f-4d75-9622-a2ebfbbac045" 
+  } 
+} 
