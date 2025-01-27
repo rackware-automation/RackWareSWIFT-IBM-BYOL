9f81a63f2ddc32d2857e87d1af0777d698f19a82
rackware-automation <106510095+rackware-automation@users.noreply.github.com>
2025-01-27 20:06:14 +0530

    Update variables.tf for 1.3.39.64

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 # }
 
 variable "image_url" {
-  default     = "cos://us-east/swift-marketplace-img/RackWareSWIFT_BYOL_ibm_1.3.33.60.qcow2"
+  default     = "cos://us-east/swift-marketplace-img/RackWareSWIFT_BYOL_ibm_1.3.39.64.qcow2"
   description = "URL for source VSI image used to spin up instance."
 }
 
